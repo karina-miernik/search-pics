4e938766b4e998ad0637abb7faafdaea2ab0cd3c
Karina Miernik <karina.miernik@gmail.com>
2020-07-14 14:32:24 +0200

    axios added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get("https://api.unsplash.com/search/photos?query=myterm", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID PXeMl76xxVl5z3fyfCtON6sZKuQS2-T15gRyDG_mZB0",
+      },
+    });
   }
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '5rem' }}>
+      <div className="ui container" style={{ marginTop: "5rem" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
